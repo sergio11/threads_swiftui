de3f51d24b74bc9292d6b98671341893df7c1653
Sergio Sánchez <sss4esob@gmail.com>
2024-07-16 21:04:07 +0200

    Initial commit - project setup

---
+++ Threads/App/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 16/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
