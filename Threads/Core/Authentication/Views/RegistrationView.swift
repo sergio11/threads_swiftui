d3911e06c9b818db7ad12f460b8eee23bfdd6b79
Sergio Sánchez <sss4esob@gmail.com>
2024-07-16 21:31:12 +0200

    Implement RegistrationView

---
+++ Threads/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,97 @@
+//
+//  RegistrationView.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 16/7/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var fullname = ""
+    @State private var username = ""
+    
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                Image("threads-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter you password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    TextField("Enter your full name", text: $fullname)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    TextField("Enter your username", text: $username)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Sign Up")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                }.padding(.vertical)
+                
+                Spacer()
+                
+                Divider()
+                
+                Button {
+                    
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Already have an account?")
+                        Text("Sign In")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundColor(.black)
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
