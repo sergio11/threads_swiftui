db6096ca0d0fc1c24ceb35b31fedfa076d8f6644
Sergio Sánchez <sss4esob@gmail.com>
2024-07-18 18:42:50 +0200

    Implement threads tabbar

---
+++ Threads/Core/Explore/View/ExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreView.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 18/7/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
