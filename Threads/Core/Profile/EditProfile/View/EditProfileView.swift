d3b3bf62cf6a6a2fd703be7e7b847ac59eb76388
Sergio Sánchez <sss4esob@gmail.com>
2024-07-18 20:00:01 +0200

    Implement Edit pprofile view

---
+++ Threads/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,100 @@
+//
+//  EditProfileView.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 18/7/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    
+    
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+                
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            
+                            Text("Charles Leclerc")
+                        }
+                        .font(.footnote)
+                        
+                        Spacer()
+                        
+                        CircularProfileImageView()
+                    }
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Enter your bio ...", text: $bio, axis: .vertical)
+                    }
+                    .font(.footnote)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Link")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Add link ...", text: $link)
+                    }
+                    .font(.footnote)
+                    
+                    Divider()
+                    
+                    Toggle("Private Profile", isOn: $isPrivateProfile)
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+                .padding()
+                
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
