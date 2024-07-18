b05092629b2015b6f76e023fc846b88b593ec339
Sergio Sánchez <sss4esob@gmail.com>
2024-07-18 18:58:29 +0200

    Implement FeedView and ThreadCell

---
+++ Threads/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,77 @@
+//
+//  ThreadCell.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 18/7/24.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                Image("test-image")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                VStack {
+                    HStack {
+                        Text("testuser")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        Spacer()
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundColor(Color(.systemGray3))
+                        Button {
+                            
+                        } label: {
+                           Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.darkGray))
+                        }
+                    }
+                    
+                    Text("Formula 1 champion")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundColor(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+        }
+    }
+}
+
+struct ThreadCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ThreadCell()
+    }
+}
