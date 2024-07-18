4f73db62cf5d358a42088c8af7fd4646a48619e5
Sergio Sánchez <sss4esob@gmail.com>
2024-07-18 19:08:38 +0200

    Implement Explore view

--- Threads/Core/Explore/View/ExploreView.swift
+++ Threads/Core/Explore/View/ExploreView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct ExploreView: View {
+    
+    @State private var searchText = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 1, id: \.self) { user in
+                        VStack {
+                            HStack {
+                                Image("test-image")
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                    .clipShape(Circle())
+                                
+                                VStack(alignment: .leading) {
+                                    Text("testuser")
+                                        .fontWeight(.semibold)
+                                    
+                                    Text("Test User")
+                                }
+                                .font(.footnote)
+                                
+                                Spacer()
+                                
+                                Text("Follow")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .frame(width: 100, height: 32)
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color(.systemGray4), lineWidth: 1)
+                                    }
+                            }
+                            .padding(.horizontal)
+                            
+                            Divider()
+                        }
+                        .padding(.vertical, 4)
+                    }
+                }
+            }
+            .navigationTitle("Seach")
+            .searchable(text: $searchText, prompt: "Search")
+        }
     }
 }
 
