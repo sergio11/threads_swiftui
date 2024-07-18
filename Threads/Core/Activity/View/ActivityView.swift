7ac6069bc2e7bf83d5e6f80f14d27dcbca99bdd0
Sergio Sánchez <sss4esob@gmail.com>
2024-07-18 18:42:50 +0200

    Implement threads tabbar

---
+++ Threads/Core/Activity/View/ActivityView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityView.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 18/7/24.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityView()
+    }
+}
---
+++ Threads/Core/ThreadCreation/View/CreateThreadView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateThreadView.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 18/7/24.
+//
+
+import SwiftUI
+
+struct CreateThreadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateThreadView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateThreadView()
+    }
+}
