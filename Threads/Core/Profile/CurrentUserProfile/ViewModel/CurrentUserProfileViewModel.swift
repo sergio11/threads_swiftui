691aab5fcc5b99a1c46d422057904ce4cee5f41c
Sergio Sánchez <sss4esob@gmail.com>
2024-07-20 21:29:05 +0200

    Implement Current profile user view

---
+++ Threads/Core/Profile/CurrentUserProfile/ViewModel/CurrentUserProfileViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  CurrentUserProfileViewModel.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 20/7/24.
+//
+
+import Foundation
+import Combine
+
+class CurrentUserProfileViewModel: ObservableObject {
+    
+    @Published var currentUser: User?
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setupSubscribers()
+    }
+    
+    private func setupSubscribers() {
+        UserService.shared.$currentUser.sink{[weak self] user in
+            self?.currentUser = user
+            print("DEBUG: User in view model from combine is \(user)")
+        }.store(in: &cancellables)
+    }
+    
+}
