15b613b18497eecb33af26c8d138b6c4142f608a
Sergio Sánchez <sss4esob@gmail.com>
2024-07-21 10:02:44 +0200

    Implement change user profile image

--- Threads/Core/Profile/EditProfile/View/EditProfileView.swift
+++ Threads/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -6,13 +6,15 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditProfileView: View {
-    
     
     @State private var bio = ""
     @State private var link = ""
     @State private var isPrivateProfile = false
+    @Environment(\.dismiss) private var onDimiss
+    @EnvironmentObject var viewModel: CurrentUserProfileViewModel
     
     var body: some View {
         NavigationStack {
@@ -32,7 +34,18 @@ struct EditProfileView: View {
                         
                         Spacer()
                         
-                        CircularProfileImageView()
+                        PhotosPicker(selection: $viewModel.selectedItem) {
+                            if let image = viewModel.profileImage {
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 40, height: 40)
+                                    .clipShape(Circle())
+                            } else {
+                                CircularProfileImageView()
+                            }
+                        }
+                        
                     }
                     
                     Divider()
@@ -75,7 +88,7 @@ struct EditProfileView: View {
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button("Cancel") {
-                        
+                        onDimiss()
                     }
                     .font(.subheadline)
                     .foregroundColor(.black)
