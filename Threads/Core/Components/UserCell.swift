6a855d22d563b311dfdb0e39b95a8baa2a6043b0
Sergio Sánchez <sss4esob@gmail.com>
2024-07-21 11:00:26 +0200

    loading profile image using Kingfisher

--- Threads/Core/Components/UserCell.swift
+++ Threads/Core/Components/UserCell.swift
@@ -13,7 +13,7 @@ struct UserCell: View {
     
     var body: some View {
         HStack {
-            CircularProfileImageView()
+            CircularProfileImageView(user: user, size: .small)
             
             VStack(alignment: .leading, spacing: 4) {
                 Text(user.username)
--- Threads/Core/Profile/EditProfile/View/EditProfileView.swift
+++ Threads/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -10,6 +10,7 @@ import PhotosUI
 
 struct EditProfileView: View {
     
+    let user: User
     @State private var bio = ""
     @State private var link = ""
     @State private var isPrivateProfile = false
@@ -28,7 +29,7 @@ struct EditProfileView: View {
                             Text("Name")
                                 .fontWeight(.semibold)
                             
-                            Text("Charles Leclerc")
+                            Text(user.fullname)
                         }
                         .font(.footnote)
                         
@@ -42,7 +43,7 @@ struct EditProfileView: View {
                                     .frame(width: 40, height: 40)
                                     .clipShape(Circle())
                             } else {
-                                CircularProfileImageView()
+                                CircularProfileImageView(user: user, size: .small)
                             }
                         }
                         
@@ -111,6 +112,6 @@ struct EditProfileView: View {
 
 struct EditProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        EditProfileView()
+        EditProfileView(user: dev.user)
     }
 }
