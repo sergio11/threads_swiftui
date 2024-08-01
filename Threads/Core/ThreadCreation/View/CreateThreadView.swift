75bacb7492fecea892cd2c5bfa7beaae42dfdcef
Sergio Sánchez <sss4esob@gmail.com>
2024-08-01 21:01:39 +0200

    Implement create thread view

--- Threads/Core/ThreadCreation/View/CreateThreadView.swift
+++ Threads/Core/ThreadCreation/View/CreateThreadView.swift
@@ -9,28 +9,32 @@ import SwiftUI
 
 struct CreateThreadView: View {
     
-    @State private var caption = ""
+    @StateObject var viewModel = CreateThreadViewModel()
     @Environment(\.dismiss) private var onDismiss
+    
+    private var user: User? {
+        return UserService.shared.currentUser
+    }
     
     var body: some View {
         NavigationStack {
             VStack {
                 HStack(alignment: .top) {
-                    CircularProfileImageView(user: nil, size: .small)
+                    CircularProfileImageView(user: user, size: .small)
                     
                     VStack(alignment: .leading, spacing: 4) {
                         Text("maxverstappen1")
                             .fontWeight(.semibold)
                         
-                        TextField("Start a thread ...", text: $caption, axis: .vertical)
+                        TextField("Start a thread ...", text: $viewModel.caption, axis: .vertical)
                     }
                     .font(.footnote)
                     
                     Spacer()
                     
-                    if !caption.isEmpty {
+                    if !viewModel.caption.isEmpty {
                         Button {
-                            caption = ""
+                            viewModel.caption = ""
                         } label: {
                             Image(systemName: "xmark")
                                 .resizable()
@@ -55,10 +59,13 @@ struct CreateThreadView: View {
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Post") {
-                        
+                        Task {
+                            try await viewModel.uploadThread()
+                            onDismiss()
+                        }
                     }
-                    .opacity(caption.isEmpty ? 0.5 : 1.0)
-                    .disabled(caption.isEmpty)
+                    .opacity(viewModel.caption.isEmpty ? 0.5 : 1.0)
+                    .disabled(viewModel.caption.isEmpty)
                     .font(.subheadline)
                     .fontWeight(.semibold)
                     .foregroundColor(.black)
---
+++ Threads/Services/ThreadService.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadService.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 1/8/24.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct ThreadService {
+    
+    static func uploadThread(_ thread: Thread) async throws {
+        guard let threadData = try? Firestore.Encoder().encode(thread) else { return }
+        try await Firestore.firestore().collection("threads").addDocument(data: threadData)
+    }
+}
