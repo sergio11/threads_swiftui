5fa5fcc36636ffbe0ea5bd24681d69922139239c
Sergio Sánchez <sss4esob@gmail.com>
2024-11-19 18:33:02 +0100

    Implement FeedView

---
+++ Threads/ViewModel/FeedViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  FeedViewModel.swift
+//  Threads
+//
+//  Created by Sergio Sánchez Sánchez on 1/8/24.
+//
+
+import Foundation
+import Combine
+import Factory
+
+@MainActor
+class FeedViewModel: BaseViewModel {
+    
+    @Injected(\.fetchThreadsUseCase) private var fetchThreadsUseCase: FetchThreadsUseCase
+    
+    @Published var threads = [ThreadBO]()
+    
+    func fetchThreads() {
+        executeAsyncTask({
+            return try await self.fetchThreadsUseCase.execute()
+        }) { [weak self] (result: Result<[ThreadBO], Error>) in
+            guard let self = self else { return }
+            switch result {
+            case .success(let threads):
+                self.onFetchThreadsCompleted(threads: threads)
+            case .failure:
+                self.onFetchThreadsFailed()
+            }
+        }
+    }
+    
+    private func onFetchThreadsCompleted(threads: [ThreadBO]) {
+        self.isLoading = false
+        self.threads = threads
+    }
+
+    private func onFetchThreadsFailed() {
+        self.isLoading = false
+    }
+}
