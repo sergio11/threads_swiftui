c3ac1e788ace4978332f233d23d76d41026cd0d3
Sergio Sánchez <sss4esob@gmail.com>
2024-11-20 19:33:53 +0100

    Implement several improvements in explore view

--- Threads/View/Core/Components/UserCell.swift
+++ Threads/View/Core/Components/UserCell.swift
@@ -10,36 +10,56 @@ import SwiftUI
 struct UserCell: View {
     
     let user: UserBO
+    let isFollowing: Bool
     
     var body: some View {
-        HStack {
-            CircularProfileImageView(profileImageUrl: user.profileImageUrl, size: .small)
+        HStack(spacing: 16) {
+            CircularProfileImageView(profileImageUrl: user.profileImageUrl, size: .medium)
+                .frame(width: 50, height: 50)
             
+            // User Info (Username, Fullname)
             VStack(alignment: .leading, spacing: 4) {
                 Text(user.username)
-                    .fontWeight(.semibold)
+                    .font(.headline) // Larger font for username
+                    .foregroundColor(.primary)
+                    .lineLimit(1)
                 
                 Text(user.fullname)
+                    .font(.subheadline) // Smaller font for fullname
+                    .foregroundColor(.secondary)
+                    .lineLimit(1)
             }
-            .font(.footnote)
             
             Spacer()
             
-            Text("Follow")
-                .font(.subheadline)
-                .fontWeight(.semibold)
-                .frame(width: 100, height: 32)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color(.systemGray4), lineWidth: 1)
-                }
+            // Follow/Unfollow Button
+            Button(action: {
+                
+            }) {
+                Text(isFollowing ? "Following" : "Follow")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(isFollowing ? .white : .blue)
+                    .padding(.vertical, 8)
+                    .frame(width: 100)
+                    .background(isFollowing ? Color.blue : Color.white)
+                    .cornerRadius(8)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(isFollowing ? Color.blue : Color(.systemGray4), lineWidth: 1)
+                    }
+            }
+            .padding(.trailing)
         }
         .padding(.horizontal)
+        .background(Color.white)
     }
 }
 
 struct UserCell_Previews: PreviewProvider {
     static var previews: some View {
-        UserCell(user: dev.user)
+        UserCell(user: dev.user, isFollowing: false)
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
